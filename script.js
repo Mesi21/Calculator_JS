88f451b29615f358d8bff8941253fa80696a5e4b
Mesi21 <mse212002@gmail.com>
2020-10-06 20:38:04 +0300

    :contruction: completed appendNum and updateDisplay functions, added event listeners to op buttons

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const numBtns = document.querySelectorAll('[data-number]');
-const opBtns = document.querySelectorAll('[data-operation]');
-const eqBtn = document.getElementById('eq');
-const acBtn = document.getElementById('ac');
-const delBtn = document.getElementById('del');
-const prevOp = document.querySelector('[data-prev-op]');
-const currOp = document.querySelector('[data-curr-op]');
-
 class Calculator {
-    constructor(prevOp, currOp) {
-        this.prevOp = prevOp;
-        this.currOp = currOp;
-        this.clear;
+    constructor(prevOpElem, currOpElem) {
+        this.prevOpElem = prevOpElem;
+        this.currOpElem = currOpElem;
+        this.clear();
     }
 
     clear = () => {
@@ -24,11 +16,18 @@ class Calculator {
     }
 
     appendNum = (num) => {
-        this.currOp = num;
+        if(num === '.' && this.currOp.includes('.')) return;
+        this.currOp = this.currOp.toString() + num.toString();
     }
 
     chooseOp = (op) => {
-
+        if (this.currOp === "") return;
+        if (this.prevOp !== "") {
+            this.compute();
+        }
+        this.op = op;
+        this.prevOp = this.currOp;
+        this.currOp = '';
     }
 
     compute = () => {
@@ -36,14 +35,31 @@ class Calculator {
     }
 
     updateDisplay = () => {
-        this.currOp.innerText = currOp;
+        this.currOpElem.innerText = this.currOp;
+        this.prevOpElem.innerText = this.prevOp;
     }
 }
 
-const calculator = new Calculator(prevOp, currOp)
+const numBtns = document.querySelectorAll('[data-number]');
+const opBtns = document.querySelectorAll('[data-operation]');
+const eqBtn = document.getElementById('eq');
+const acBtn = document.getElementById('ac');
+const delBtn = document.getElementById('del');
+const prevOpElem = document.querySelector('[data-prev-op]');
+const currOpElem = document.querySelector('[data-curr-op]');
+
+const calculator = new Calculator(prevOpElem, currOpElem)
+
 numBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         calculator.appendNum(btn.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+opBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.chooseOp(btn.innerText);
         calculator.updateDisplay();
     })
 })
